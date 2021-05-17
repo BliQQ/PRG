23e0944f4c2a3537703b7a2727cc91dbfc578a42
BliQQ <45635246+BliQQ@users.noreply.github.com>
2021-05-17 13:32:23 +0200

    Add files via upload

---
+++ JAVA_NHL/src/test2/Team.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test2;
+
+/**
+ *
+ * @author beh01
+ */
+public class Team implements Comparable<Team> {
+
+    private String name;
+    private int score;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public void win(){
+        this.score += 3;
+    }
+    public void winInOvertime(){
+        this.score += 2;
+    }
+    public void lostInOvertime(){
+        this.score += 1;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public int compareTo(Team o) {
+        return this.getName().compareTo(o.getName());
+    }
+
+
+}
